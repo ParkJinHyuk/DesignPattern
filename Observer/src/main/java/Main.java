7f1794aae8b3ff9dc42108068f12d9388c65a96f
Parkjinhyuk <jinhyuk8007@gmail.com>
2020-02-18 13:56:38 +0900

    observer pattern example

---
+++ Observer/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionDisplay currentConditionDisplay = new CurrentConditionDisplay(weatherData);
+        weatherData.setMeasurements(80, 65, 30.4f);
+    }
+}
